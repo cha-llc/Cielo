ed5f3ffd1fa553ffab9ec022e53f7a2b3885b5cd
CJ Adisa <chaholdingsltd@gmail.com>
2026-01-17 15:57:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2539723907-d7516",
+  "appId": "1:811456090413:web:3074b0ee2cef882d2b43a4",
+  "apiKey": "AIzaSyCigrz7AkS8SVKJ9XHh2_QKLINMNWX2RA8",
+  "authDomain": "studio-2539723907-d7516.firebaseapp.com",
+  "measurementId": "G-0Y8Q0D5KZF",
+  "messagingSenderId": "811456090413"
+};
