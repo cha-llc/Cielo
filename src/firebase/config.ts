264dda356e6347451371b0038ce50f54603a78f8
Tea Time Network <cjhadisa@icloud.com>
2026-01-24 17:25:27 -0500

    Update Firebase configuration with new values

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-2539723907-d7516",
-  "appId": "1:811456090413:web:3074b0ee2cef882d2b43a4",
-  "apiKey": "AIzaSyCigrz7AkS8SVKJ9XHh2_QKLINMNWX2RA8",
-  "authDomain": "studio-2539723907-d7516.firebaseapp.com",
-  "measurementId": "G-0Y8Q0D5KZF",
-  "messagingSenderId": "811456090413"
+  apiKey: "AIzaSyCigrz7AkS8SVKJ9XHh2_QKLINMNWX2RA8",
+  authDomain: "studio-2539723907-d7516.firebaseapp.com",
+  projectId: "studio-2539723907-d7516",
+  storageBucket: "studio-2539723907-d7516.firebasestorage.app",
+  messagingSenderId: "811456090413",
+  appId: "1:811456090413:web:3e5f95c8f4f0c8b42b43a4",
+  measurementId: "G-KV3E67NZG7"
 };
